817d219a23dda2c07d5bcfc5a572fb43fb54c58f
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-10 02:01:30 +0530

    Added:Editing contact details as per user

--- AddressBook.java
+++ AddressBook.java
@@ -43,4 +43,93 @@ class AddressBook
 								+info.state+" "+info.email+" "+info.zip+" "+info.phn);
 		}
 	}
+
+	void editContact()
+	{
+		firstName=sc.next();
+		int flag=0;
+		for(int i=0;i<contact.size();i++)
+		{
+			info=contact.get(i);
+			if(firstName.equals(info.firstName))
+			{
+				System.out.println();
+				System.out.println("Do u want to edit entire contact details? yes or no");
+				String ch=sc.next();
+				if(ch.equals("yes"))
+				{
+					System.out.println("Enter your new address:");
+					address=sc.next();
+					System.out.println("Enter your new city:");
+					city=sc.next();
+					System.out.println("Enter your new state:");
+					state=sc.next();
+					System.out.println("Enter your new email:");
+					email=sc.next();
+					System.out.println("Enter your new zip:");
+					zip=sc.nextInt();
+					System.out.println("Enter your new phn number:");
+					phn=sc.nextLong();
+					contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+					flag=1;
+				}
+				else
+				{
+					System.out.println();
+					System.out.println("Enter your choice to edit particular details.:1.firstname 2.lastname 3.address 4.city 5.state 6.email 7.zip 8.phn number ");
+					int choice=sc.nextInt();
+					switch(choice)
+					{
+						case 1:System.out.println("Enter your new first name:");
+							   firstName=sc.next();
+							   contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+							   break;
+						case 2:System.out.println("Enter your new last name:");
+								lastName=sc.next();
+								contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+								break;
+						case 3:System.out.println("Enter your new address:");
+								address=sc.next();
+								contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+								break;
+						case 4:System.out.println("Enter your new city:");
+								city=sc.next();
+								contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+								break;
+						case 5:System.out.println("Enter your new state:");
+								state=sc.next();
+								contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+								break;
+						case 6:System.out.println("Enter your new email:");
+								email=sc.next();
+								contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+								break;
+						case 7:System.out.println("Enter your new zip");
+								zip=sc.nextInt();
+								contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+								break;
+						case 8:System.out.println("Enter your new phn number");
+								phn=sc.nextLong();
+								contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+								break;
+
+					}
+					flag=1;
+				}
+				System.out.println();
+				System.out.println("Address book after editing details:");
+				System.out.println("-------------------------");
+				for(i=0;i<number;i++)
+				{
+					info=contact.get(i);
+					System.out.println(info.firstName+" "+info.lastName+" "+info.address+" "+info.city+" "
+								+info.state+" "+info.email+" "+info.zip+" "+info.phn);
+				}
+				if(flag==0)
+				System.out.println("Given name is not found in address book..");
+
+			}
+			
+		}
+	}
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -9,5 +9,14 @@ class AddressBookMain
 		System.out.println();
 		System.out.println("How many contacts u want to enter:");
 		book.addContact();
+		System.out.println();
+		System.out.println("Do you want to edit the contact? yes or no ");
+		String option=sc.next();
+		if(option.equals("yes"))
+		{
+			System.out.println();
+			System.out.println("Enter first name to edit:");
+			book.editContact();
+		}
 	}
 }
