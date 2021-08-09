e0d43191875a23999f4d1d5a9b1d5d555ce8fc80
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-10 01:39:26 +0530

    Added:created to a new contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 class AddressBookMain
 {
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Address book program..");
+		AddressBook book=new AddressBook();
+		Scanner sc=new Scanner(System.in);
+		System.out.println();
+		System.out.println("How many contacts u want to enter:");
+		book.addContact();
 	}
 }
